28e2550197e4f0cece44157940643a97b69a98f4
Mark Tranter <mark.a.tranter@gmail.com>
2019-07-05 19:41:00 +1000

    Add bintray deets

--- project/Common.scala
+++ project/Common.scala
@@ -1,15 +1,19 @@
+import bintray.BintrayPlugin.autoImport.{bintrayOrganization, bintrayPackageLabels}
 import sbt._
 import sbt.Keys.{licenses, _}
 
 object Common {
-
-  val scalaV211 = "2.11.12"
-  val scalaV212 = "2.12.8"
-
-  def settings(): Seq[Def.Setting[_]] = Seq(
-    scalaVersion := scalaV212,
-    version := "0.1",
-    crossScalaVersions := Seq(scalaV211, scalaV212),
+  def apply() = Seq(
+    scalaVersion := "2.12.8",
+    organization := "com.engitano",
+    organizationName := "Engitano",
+    startYear := Some(2019),
+    bintrayOrganization := Some("engitano"),
+    bintrayPackageLabels := Seq("firestore", "fs2"),
     licenses += ("MIT", new URL("http://opensource.org/licenses/MIT")),
+    scalacOptions := Seq(
+      "-Ypartial-unification"
+    )
   )
+
 }
